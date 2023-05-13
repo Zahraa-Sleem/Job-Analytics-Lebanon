f79ae5c66cac8795d8882829216da406ac89a1f4
Zahraa Sleem <zahraasleem03@gmail.com>
2023-05-13 19:07:11 +0300

    Cleaned and sorted location

--- datacleaning.py
+++ datacleaning.py
@@ -3,5 +3,9 @@ with open('objects.json') as f:
     data = json.load(f)
 for element in data:
     element['jobtitle']=element['jobtitle'].replace("/n",'').strip()
+    if ',' in element['location']:
+        element['location']=element['location'].split(",")[1]
+        print(element['location'])
 with open("objects.json", "w") as f:
-    json.dump(data, f, indent=2)   
+    json.dump(data, f, indent=2)  
+ 
