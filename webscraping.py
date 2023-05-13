409f2b645e5674b73c69d23ee78255fc87871e14
Zahraa Sleem <zahraasleem03@gmail.com>
2023-05-13 19:05:21 +0300

    Fixed Names

--- webscraping.py
+++ webscraping.py
@@ -15,25 +15,45 @@ while True:
             break
         except:
             pass
-while True:
+check=True
+count=0
+while check:
+    while True:
         try:
-            parent_element=driver.find_element(By.XPATH, '//*[@id="results"]/div[1]/div[2]')
+            a_tag= driver.find_element(By.XPATH,"//a[text()='Load More Jobs']")
+            a_tag.click()
+            count+=1
+            break
+        except:
+            try:
+                more= driver.find_element(By.CLASS_NAME,"data-notfound")
+                check=False
+                break
+            except:
+                pass
+child_inner_html_list = []
+for j in range(2,count+2):
+    while True:
+        try:
+            parent_element=driver.find_element(By.XPATH, f'//*[@id="results"]/div[1]/div[{j}]')
             break
         except:
             pass
-child_inner_html_list = []
-child_elements = parent_element.find_elements(By.XPATH,"./*")
-for child in child_elements:
-    inner_html = child.get_attribute("innerHTML")
-    child_inner_html_list.append(inner_html)
-child_dict = {
-    "children": child_inner_html_list
-}
-with open("child_elements.json", "w") as f:
-    json.dump(child_dict, f, indent=2)   
+    child_elements = parent_element.find_elements(By.XPATH,"./*")
+    i=1
+    for child in child_elements:
+        try:
+            element={ 
+            "department":child.find_element(By.XPATH,f'//*[@id="results"]/div[1]/div[{j}]/div[{i}]/div/div[1]/span/span').get_attribute("innerHTML"),
+            "location":child.find_element(By.XPATH,f'//*[@id="results"]/div[1]/div[{j}]/div[{i}]/div/div[2]/div/span/span').get_attribute("innerHTML"),
+            "jobtitle":child.find_element(By.XPATH,f'//*[@id="results"]/div[1]/div[{j}]/div[{i}]/div/div[2]/a').get_attribute("innerHTML"),
+            "time":child.find_element(By.XPATH,f'//*[@id="results"]/div[1]/div[{j}]/div[{i}]/div/div[3]/span[1]').get_attribute("innerHTML")
+            }
+            # print(element)
+            child_inner_html_list.append(element)
+            i=i+1
+        except:
+            pass
 
-
-
-
-
-
+with open("objects.json", "w") as f:
+    json.dump(child_inner_html_list, f, indent=2)   
