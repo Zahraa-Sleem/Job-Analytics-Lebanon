14ef156295c1f6bbbda7bdd33c13913c77a817d1
Zahraa Sleem <zahraasleem03@gmail.com>
2023-05-19 19:39:16 +0300

    Added Tags

--- datacleaning.py
+++ datacleaning.py
@@ -1,11 +1,20 @@
 import json
 with open('objects.json') as f:
     data = json.load(f)
+with open('bagofwords.json') as b:
+    bag=json.load(b)
 for element in data:
     element['jobtitle']=element['jobtitle'].replace("/n",'').strip()
     if ',' in element['location']:
         element['location']=element['location'].split(",")[1]
         print(element['location'])
+    element["tags"]=[]
+    words=element['jobtitle'].split()
+    for item in bag:
+        common_elements = set(map(str.lower, words)).intersection(map(str.lower, item['Keywords']))
+        if(len(common_elements)):
+          element["tags"].append(item["tag"])  
+     
 with open("objects.json", "w") as f:
     json.dump(data, f, indent=2)  
  
